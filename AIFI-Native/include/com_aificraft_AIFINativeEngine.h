f44657a80982f694fba5c6ce043976303af73097
greeko2030-afk <greeko2030@gmail.com>
2026-07-17 21:58:15 +0600

    Update com_aificraft_AIFINativeEngine.h

--- AIFI-Native/include/com_aificraft_AIFINativeEngine.h
+++ AIFI-Native/include/com_aificraft_AIFINativeEngine.h
@@ -1,1 +1,18 @@
+#include <jni.h>
 
+#ifndef _Included_com_aificraft_AIFINativeEngine
+#define _Included_com_aificraft_AIFINativeEngine
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT void JNICALL Java_com_aificraft_AIFINativeEngine_processFrame
+  (JNIEnv *, jclass, jlong, jint, jint);
+
+JNIEXPORT void JNICALL Java_com_aificraft_AIFINativeEngine_renderInterpolatedFrame
+  (JNIEnv *, jclass);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
