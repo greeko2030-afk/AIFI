cdc2f4d078344337a84e5a61d8bc4bbe01a9c4e7
greeko2030-afk <greeko2030@gmail.com>
2026-07-18 10:33:28 +0600

    Update AIFINativeEngine.java

--- src/main/java/com/aificraft/AIFINativeEngine.java
+++ src/main/java/com/aificraft/AIFINativeEngine.java
@@ -1,1 +1,10 @@
+package com.aificraft;
+
+public class AIFINativeEngine {
+    // Sends frame data pointer to C++
+    public static native void processFrame(long frameBufferPointer, int width, int height);
+    
+    // Tells C++ to render the AI generated frame
+    public static native void renderInterpolatedFrame();
+}
 
